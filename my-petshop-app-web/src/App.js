e89ac5244fa222d324a1bef2e2c671a5aba9c47f
Carosb09 <bernalcarolina163@gmail.com>
2024-11-02 21:44:48 -0500

    Front

--- my-petshop-app-web/src/App.js
+++ my-petshop-app-web/src/App.js
@@ -1,23 +1,17 @@
 import { useState, useEffect } from "react"
 import FirstComponent from "./Componentes/FirstComponent"
-import { response } from "express"
 
 function App() {
 
   useEffect(()=>{
-    console.log("Ejecutandose") 
-  fetch('https://telematica-nodejs.onrender.com', {
-    method: "GET"
-  }).then(response => response.jason()
-  .then(data => console.log(data))
-  .catch(err = console.log(err))
-)
-  .catch(errorResponse => console.log(errorResponse))
-}, [])
+    console.log("Ejecutandose")
+  }, [])
+
   return (
     <>
     <FirstComponent></FirstComponent>
-    <FirstComponent />
+    <FirstComponent /> 
+    <FirstComponent /> 
     </>
   );
 }
