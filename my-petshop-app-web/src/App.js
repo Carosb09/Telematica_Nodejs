0840cecfd6c512ba6ebba2a2d4e82cb4237d1260
Carosb09 <bernalcarolina163@gmail.com>
2024-11-11 12:55:11 -0500

    Arreglo

--- my-petshop-app-web/src/App.js
+++ my-petshop-app-web/src/App.js
@@ -1,17 +1,35 @@
-import { useState, useEffect } from "react"
-import FirstComponent from "./Componentes/FirstComponent"
+import { useState, useEffect } from "react";
+import FirstComponent from "./Componentes/FirstComponent";
+import "./Styles.css"
 
 function App() {
+  const[Products, setProducts] = useState([])
+  useEffect(() => {
+    console.log("Ejecutándose");
 
-  useEffect(()=>{
-    console.log("Ejecutandose")
+    fetch('https://telematica-nodejs.onrender.com/Products/', {
+      method: "GET"
+
+    }).then(response => response.json()
+      .then(data => setProducts(data))
+      .catch(err => console.log(err))
+    )
+      .catch(errorResponse => console.log(errorResponse))
   }, [])
 
   return (
     <>
-    <FirstComponent></FirstComponent>
-    <FirstComponent /> 
-    <FirstComponent /> 
+      <FirstComponent></FirstComponent>
+      <FirstComponent />
+      {Products.length === 0 ?
+      <h3>No hay productos registrados</h3>:
+      Products.map(Products => <div className="fondo">
+      <h3>ID: {Products.id}</h3>
+      <p>Producto: {Products.Nombre}</p>
+      <p>{Products.Color}</p>
+      <p>{Products.Cantidad}</p>
+      </div>)
+      }
     </>
   );
 }
